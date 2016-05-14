23ed9a3e458aa682a70cb9c5e987303e5fb4a5a4
Chris P <>
2016-05-14 15:21:08 -0400

    script crashes at 1970, reads data wrong on some mono-stereo charts

--- scanner.py
+++ scanner.py
@@ -11,6 +11,10 @@ def getSoup(url):
 def tagToText(tag):
 	return tag.find(text=True).strip()
 
+def getHref(tag):
+	return None if tag.find('a')==None else tag.find('a').get("href")
+
+
 BASE_URL=	"https://en.wikipedia.org"
 
 year=	1945
@@ -18,26 +22,32 @@ while year <= date.today().year:
 	yearURL=	BASE_URL+"/wiki/List_of_Billboard_200_number-one_albums_of_"+str(year)
 	yearPage=	getSoup(yearURL)
 
-	wikitable=			yearPage.find("table",{"class": "wikitable"})
-	wikitableChildren=	wikitable.findChildren("tr")
-	album=	None
-	artist=	None
-	label=	None	
-	for tr in wikitableChildren:
-		tdList=			tr.findChildren("td")	# `td` elements containing info on dates, album titles, artists and labels
-		tdListLength=	len(tdList)
-		
-		if tdListLength > 0:	# If there are `td` elements, we can be sure we are reading data about the Billboard 200 
-			chartDate=		tdList[0].find(text=True).strip() + ' ' + str(year)
-			chartDatePython=datetime.strptime(chartDate,"%B %d %Y").date()
+	wikitables=			yearPage.findAll("table",{"class": "wikitable"})
+	for wikitable in wikitables:
+		wikitableChildren=	wikitable.findChildren("tr")
+		if len(wikitableChildren) <= 1:
+			continue
 
-			tdHasAlbumInfo=	tdListLength == 4
-			if tdHasAlbumInfo:
-				album=	tagToText(tdList[1])
-				artist=	tagToText(tdList[2])
-				label=	tagToText(tdList[3])
-				
-			print chartDatePython, album, artist, label
+		album=	None
+		artist=	None
+		label=	None	
 
+		for tr in wikitableChildren:
+			tdList=			tr.findChildren("td")	# `td` elements containing info on dates, album titles, artists and labels
+			tdListLength=	len(tdList)
+			if tdListLength > 0:	# If there are `td` elements, we can be sure we are reading data about the Billboard 200 
+				chartDate=		tdList[0].find(text=True).strip() + ' ' + str(year)
+				chartDatePython=datetime.strptime(chartDate,"%B %d %Y").date()
+				tdHasAlbumInfo=	tdListLength > 1	# `tdListLength` is `1` when only the date is available
+				if tdHasAlbumInfo:
+					album=		tagToText(tdList[1])
+					albumHref=	getHref(tdList[1])
+					if tdListLength > 2:
+						artist=		tagToText(tdList[2])
+						artistHref=	getHref(tdList[2])
+
+				print chartDatePython, album, albumHref, artist, artistHref
+				# print "=="
+
+	print "==="
 	year += 1
-	break
