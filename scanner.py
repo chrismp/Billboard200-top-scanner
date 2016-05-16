fb911817807768194fb1bfe64a9da89b974ac36c
EC2 Default User <ec2-user@ip-172-31-61-71.ec2.internal>
2016-05-16 17:17:11 +0000

    fixed header-writing bug, allow CSV to be read by git

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 from datetime import date, datetime
+import csv
 
 def getSoup(url):
 	print "Opening "+url
@@ -19,43 +20,63 @@ def getDate(tag):
 	return tag.find(text=True).strip() + ' ' + str(year)
 
 
-BASE_URL=	"https://en.wikipedia.org"
-year=		1945
-while year <= date.today().year:
-	year=	1964 if year==1960 else year
-	yearURL=	BASE_URL+"/wiki/List_of_Billboard_200_number-one_albums_of_"+str(year)
-	yearPage=	getSoup(yearURL)
+outputFile=	"Billboard200Data.csv"
+with open(outputFile, "wb") as csvFile:
+	headers=	[
+				"Year",
+				"Album",
+				"AlbumURL",
+				"Artist",
+				"ArtistURL"
+			]
+	writer=		csv.DictWriter(csvFile, fieldnames=headers, quotechar='"' )
 
-	wikitables=	yearPage.findAll("table",{"class": "wikitable"})
-	for wikitable in wikitables:
-		wikitableChildren=	wikitable.findChildren("tr")
+	writer.writeheader()
 
-		if len(wikitableChildren) <= 1:
-			continue
+	BASE_URL=	"https://en.wikipedia.org"
+	year=		1945
+	while year <= date.today().year:
+		year=	1964 if year==1960 else year	# 1960-63 charts are split between mono and stereo sound. I have not yet figured out how to make my script scrape ONLY data from the mono charts.
+		yearURL=	BASE_URL+"/wiki/List_of_Billboard_200_number-one_albums_of_"+str(year)
+		yearPage=	getSoup(yearURL)
 
-		album=	None
-		artist=	None
-		label=	None
-		for tr in wikitableChildren:
-			tdList=			tr.findChildren("td")	# `td` elements containing info on dates, album titles, artists and labels
-			tdListLength=	len(tdList)
-			if tdListLength > 0:	# If there are `td` elements, we can be sure we are reading data about the Billboard 200 
-				if tagToText(tdList[0]) == "Issue Date":
-					continue
+		wikitables=	yearPage.findAll("table",{"class": "wikitable"})
+		for wikitable in wikitables:
+			wikitableChildren=	wikitable.findChildren("tr")
 
-				chartDate=	getDate(tr.findChildren("th")[0]) if len(tr.findChildren("th"))==1 else getDate(tdList[0])	# This ternary statement is necessary because startng with 2014, date is stored in `th`
-				chartDatePython=datetime.strptime(chartDate,"%B %d %Y").date()
-				tdHasAlbumInfo=	tdListLength > 1	# `tdListLength` is `1` when only the date is available
-				if tdHasAlbumInfo:					
-					if tdListLength > 2:
-						albumTag=	tdList[1] if year < 2014 else tdList[0]
-						album=		tagToText(albumTag)
-						albumHref=	getHref(albumTag)
-						artistTag=	tdList[2] if year < 2014 else tdList[1]
-						artist=		tagToText(artistTag)
-						artistHref=	getHref(artistTag)
-				print chartDatePython, album, albumHref, artist, artistHref
-				# print "=="
+			if len(wikitableChildren) <= 1:
+				continue
 
-	print "==="
-	year += 1
+			album=	None
+			artist=	None
+			label=	None
+			for tr in wikitableChildren:
+				tdList=			tr.findChildren("td")	# `td` elements containing info on dates, album titles, artists and labels
+				tdListLength=	len(tdList)
+				if tdListLength > 0:	# If there are `td` elements, we can be sure we are reading data about the Billboard 200 
+					if tagToText(tdList[0]) == "Issue Date":
+						continue
+
+					chartDate=	getDate(tr.findChildren("th")[0]) if len(tr.findChildren("th"))==1 else getDate(tdList[0])	# This ternary statement is necessary because startng with 2014, date is stored in `th`
+					chartDatePython=datetime.strptime(chartDate,"%B %d %Y").date()
+					tdHasAlbumInfo=	tdListLength > 1	# `tdListLength` is `1` when only the date is available
+					if tdHasAlbumInfo:
+						if tdListLength > 2:
+							albumTag=	tdList[1] if year < 2014 else tdList[0]
+							album=		tagToText(albumTag)
+							albumHref=	getHref(albumTag)
+							artistTag=	tdList[2] if year < 2014 else tdList[1]
+							artist=		tagToText(artistTag)
+							artistHref=	getHref(artistTag)
+					writer.writerow(
+						{
+							"Year": year,
+							"Album": album,
+							"AlbumURL": albumHref,
+							"Artist": artist,
+							"ArtistURL": artistHref
+						}
+					)
+					print chartDatePython, album, albumHref, artist, artistHref
+		print "==="
+		year += 1
